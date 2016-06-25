1b9c4a5a7eebfd2495deda5ee8317e8f8bd33a31
Dmitriy Dubson <d.dubson@gmail.com>
2016-06-25 11:49:39 -0400

    Add more Go

---
+++ taxcalculator.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strconv"
+)
+
+func main() {
+	input := bufio.NewScanner(os.Stdin)
+	// declare variables, initialized to default
+	var mealCost, tipPercent, taxPercent float64
+
+	if input.Scan() {
+		// convert string to float 64-bit
+		mealCost, _ = strconv.ParseFloat(input.Text(), 64)
+	}
+
+	if input.Scan() {
+		tipPercent, _ = strconv.ParseFloat(input.Text(), 64)
+	}
+
+	if input.Scan() {
+		taxPercent, _ = strconv.ParseFloat(input.Text(), 64)
+	}
+
+	tip := mealCost * (tipPercent / 100)
+	tax := mealCost * (taxPercent / 100)
+	fmt.Printf("The total meal cost is %d dollars.\n", int(mealCost + tip + tax + 0.5))
+}
